d27c1a6c20a9efc12325363496bee4ff5c62703f
t.voteva <t.voteva@qiwi.com>
2019-03-18 21:56:45 +0300

    Filter canteens by building

--- src/components/CanteensList.js
+++ src/components/CanteensList.js
@@ -3,30 +3,41 @@ import { Link } from 'react-router-dom';
 import './CanteensList.css';
 
 const CanteensList = ({ canteens }) => (
-  <div>
+  <div className="canteens-list">
     <h1>ГЗ</h1>
     <section className="flex-container">
-      {canteens.map(canteen => (
-        <div className="flex-item canteens-list-item">
-          <Link key={canteen.canteenUid} to={`/canteens/${canteen.canteenUid}`}>
-            <div className="canteen-img-container">
-              <img src={canteen.imageUrl} alt={canteen.name} className="canteen-img" />
-              <div className="canteen-name">{canteen.name}</div>
-            </div>
-            <div>
-              <span className="canteen-location">{canteen.location}</span>
-              <span className="canteen-working-hours">
-                с {canteen.workFromMillis} до {canteen.workTillMillis}
-              </span>
-              <span className="canteen-status-open">Открыто</span>
-            </div>
-          </Link>
-        </div>
-      ))}
+      {canteens
+        .filter(canteen => canteen.building === `GZ`)
+        .map(canteen => (
+          <CanteensListItem canteen={canteen} />
+        ))}
     </section>
-    <br />
     <h1>УЛК</h1>
-    <br />
+    <section className="flex-container">
+      {canteens
+        .filter(canteen => canteen.building === `ULK`)
+        .map(canteen => (
+          <CanteensListItem canteen={canteen} />
+        ))}
+    </section>
+  </div>
+);
+
+const CanteensListItem = ({ canteen }) => (
+  <div className="flex-item canteens-list-item">
+    <Link key={canteen.canteenUid} to={`/canteens/${canteen.canteenUid}`}>
+      <div className="canteen-img-container">
+        <img src={canteen.imageUrl} alt={canteen.name} className="canteen-img" />
+        <div className="canteen-name">{canteen.name}</div>
+      </div>
+      <div>
+        <span className="canteen-location">{canteen.location}</span>
+        <span className="canteen-working-hours">
+          с {canteen.workFromMillis} до {canteen.workTillMillis}
+        </span>
+        <span className="canteen-status-open">Открыто</span>
+      </div>
+    </Link>
   </div>
 );
 
--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Avenir Next', 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `;
 
