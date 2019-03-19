468ec128601825297e9b00e8dc78ac7c0af88ba1
t.voteva <t.voteva@qiwi.com>
2019-03-20 00:45:35 +0300

    Fix paddings

--- src/components/CanteensListStyled.js
+++ src/components/CanteensListStyled.js
@@ -5,14 +5,12 @@ import { formatTime, isOpen } from '../utils/timeUtils';
 import ItemImageContainer from './common/ItemImageContainer';
 import FlexContainer from './common/FlexContainer';
 import FlexItem from './common/FlexItem';
-import ItemInfoContainer from './common/ItemInfoContainer';
 import SubHeader from './common/SubHeader';
 
 const buildings = [{ id: `GZ`, name: `ГЗ` }, { id: `ULK`, name: `УЛК` }];
 
 const CanteenStatusOpen = styled.div`
   float: right;
-  display: inline;
   padding: 2px 12px;
   border: 1px solid lightgreen;
   border-radius: 8px;
@@ -21,37 +19,64 @@ const CanteenStatusOpen = styled.div`
 
 const CanteenStatusClosed = styled.div`
   float: right;
-  display: inline;
   padding: 2px 12px;
   border: 1px solid red;
   border-radius: 8px;
   color: red;
 `;
 
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Location = styled.div`
+  display: block;
+  font-size: 2rem;
+  font-weight: bold;
+  color: grey;
+`;
+
+const WorkingHours = styled.div`
+  color: grey;
+`;
+
+const TimeInfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const CanteensListItem = ({ canteen }) => (
-  <Link key={canteen.canteenUid} to={`/canteens/${canteen.canteenUid}`}>
-    <ItemImageContainer>
-      <img src={canteen.imageUrl} alt={canteen.name} />
-      <span>{canteen.name}</span>
-    </ItemImageContainer>
-    <ItemInfoContainer>
-      <h3>{canteen.location}</h3>
-      <span>
-        с {formatTime(canteen.workFromMillis)} до {formatTime(canteen.workTillMillis)}
-      </span>
-      {isOpen(canteen.workFromMillis, canteen.workTillMillis) ? (
-        <CanteenStatusOpen>Открыто</CanteenStatusOpen>
-      ) : (
-        <CanteenStatusClosed>Закрыто</CanteenStatusClosed>
-      )}
-    </ItemInfoContainer>
-  </Link>
+  <React.Fragment>
+    <Link key={canteen.canteenUid} to={`/canteens/${canteen.canteenUid}`}>
+      <ItemImageContainer>
+        <img src={canteen.imageUrl} alt={canteen.name} />
+        <span>{canteen.name}</span>
+      </ItemImageContainer>
+    </Link>
+    <InfoContainer>
+      <Location>{canteen.location}</Location>
+      <TimeInfoContainer>
+        <WorkingHours>
+          с {formatTime(canteen.workFromMillis)} до {formatTime(canteen.workTillMillis)}
+        </WorkingHours>
+        {isOpen(canteen.workFromMillis, canteen.workTillMillis) ? (
+          <CanteenStatusOpen>Открыто</CanteenStatusOpen>
+        ) : (
+          <CanteenStatusClosed>Закрыто</CanteenStatusClosed>
+        )}
+      </TimeInfoContainer>
+    </InfoContainer>
+  </React.Fragment>
 );
 
 const CanteensList = ({ canteens }) => (
   <div>
     {buildings.map(building => (
-      <div>
+      <React.Fragment>
         <SubHeader>{building.name}</SubHeader>
         <FlexContainer>
           {canteens
@@ -62,7 +87,7 @@ const CanteensList = ({ canteens }) => (
               </FlexItem>
             ))}
         </FlexContainer>
-      </div>
+      </React.Fragment>
     ))}
   </div>
 );
--- src/components/common/FlexItem.js
+++ src/components/common/FlexItem.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 
 const FlexItem = styled.div`
-  margin: 10px 10px;
-  width: calc(33% - 10 * 2px);
+  margin: 0 50px 32px 0;
+  width: calc(33% - 10 * 5px);
 
   @media (max-width: 800px) and (min-width: 500px) {
-    width: calc(50% - 3 * 2px);
+    margin: 0 16px 24px;
+    width: calc(50% - 2 * 16px);
   }
 
   @media (max-width: 500px) {
-    width: calc(100% - 2 * 2px);
+    margin: 0 16px 16px;
+    width: calc(100% - 2 * 16px);
   }
 `;
 
